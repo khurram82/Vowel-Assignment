2ac421eff402200c0c2034ef8538a161040cae2e
Khurram Shahzad <khurramo@gmail.com>
2023-03-06 15:08:59 +0500

    Add files via upload

---
+++ vowel.js
@@ -0,0 +1,10 @@
+function IsVowel() {
+    var alpha = 'b';
+    if (alpha == "a" || alpha == "e" || alpha == "i" || alpha == "o" || alpha == "u") {
+        console.log("Vowel");
+    }
+    else {
+        console.log("Is Not Vowel");
+    }
+    IsVowel();
+}
---
+++ vowel.ts
@@ -0,0 +1,16 @@
+
+
+    let alpha = 'e';
+if(alpha== "a" || alpha == "e" || alpha =="i" || alpha =="o" || alpha == "u")
+{
+console.log("Vowel");
+
+}
+else {
+    
+    console.log("Is Not Vowel");
+
+}
+
+
+
